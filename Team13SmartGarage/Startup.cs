758c432c665919e1c306ae3880e4a67be31ce3c2
Victor Vassilev <vic.vassilev@gmail.com>
2021-05-15 17:31:43 +0300

    Creating Development Branch.

--- Team13SmartGarage/Startup.cs
+++ Team13SmartGarage/Startup.cs
@@ -24,7 +24,7 @@ namespace Team13SmartGarage
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the Container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();

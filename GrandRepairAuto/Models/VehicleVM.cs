f645c40f4a617d2a258071adfb2d12ef3c160e8b
Victor Vassilev <vic.vassilev@gmail.com>
2021-06-02 23:28:21 +0300

    Fix Home Public NavBar Create Vehicle View

--- GrandRepairAuto/Models/VehicleVM.cs
+++ GrandRepairAuto/Models/VehicleVM.cs
@@ -1,13 +1,16 @@
-﻿using GrandRepairAuto.Data.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using GrandRepairAuto.Data.Enums;
 
 namespace GrandRepairAuto.Web.Models
 {
     public class VehicleVM
     {
+        [Required]
         public string RegPlate { get; set; }
 
         public int VehicleModelId { get; set; }
 
+        [Required]
         public string Vin { get; set; }
 
         public VehicleTypes VehicleType { get; set; }
--- GrandRepairAuto/ViewControllers/VehicleController.cs
+++ GrandRepairAuto/ViewControllers/VehicleController.cs
@@ -1,4 +1,10 @@
-﻿using GrandRepairAuto.Data.Enums;
+﻿using System.Collections.Generic;
+using AutoMapper;
+using GrandRepairAuto.Data.Enums;
+using GrandRepairAuto.Services;
+using GrandRepairAuto.Services.Contracts;
+using GrandRepairAuto.Services.Models.VehiclesDTOs;
+using GrandRepairAuto.Web.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +14,60 @@ namespace GrandRepairAuto.Web.ViewControllers
     [Authorize(Roles = Roles.AdminsAndEmployees)]
     public class VehicleController : Controller
     {
+        private IVehicleService vehicleService;
+        private IMapper mapper;
+
+        public VehicleController(IVehicleService vehicleService, IMapper mapper)
+        {
+            this.vehicleService = vehicleService;
+            this.mapper = mapper;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var vehicles = vehicleService.GetAll();
+            var vehiclesVMs = mapper.Map<List<VehicleVM>>(vehicles);
+            return View(vehiclesVMs);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new VehicleVM());
+        }
+
+        [HttpPost]
+        public IActionResult Create(VehicleVM vehicle)
+        {
+            VehicleCreateDTO createDTO = mapper.Map<VehicleCreateDTO>(vehicle);
+            vehicleService.Create(createDTO);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var getDTO = vehicleService.GetByID(id);
+            var viewModel = mapper.Map<VehicleVM>(getDTO);
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Update(VehicleVM vehicle, int id)
+        {
+            VehicleUpdateDTO updateDTO = mapper.Map<VehicleUpdateDTO>(vehicle);
+            vehicleService.Update(updateDTO, id);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            vehicleService.Delete(id);
+
+            return RedirectToAction("Index");
         }
     }
 }
